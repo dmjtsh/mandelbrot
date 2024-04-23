6da0dde563d699f772e20b99be2498c7d512d4cc
dmjtsh <cyber.pelmen@yandex.ru>
2024-04-23 19:09:04 +0300

    Swapped To Linux

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dmit/Desktop/PROGRAMMING/DED_2SEM/mandelbrot
-# Build directory: /home/dmit/Desktop/PROGRAMMING/DED_2SEM/mandelbrot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
